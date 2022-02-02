f9d2f808c1888a9973d6647e03387373fa172f7d
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-02-01 18:11:37 -0600

    Add list styles

--- website/source/_layouts/post.blade.php
+++ website/source/_layouts/post.blade.php
@@ -14,6 +14,10 @@
     font-size: 1.5rem;
     line-height: 2rem;
 }
+#content li {
+    list-style-type: disc;
+    margin-left: 2rem;
+}
 </style>
 <h1 class="text-4xl md:text-5xl font-bold mt-4 md:mt-16 text-center">{{ $page->title }}</h1>
 <h2 class="text-xl md:text-2xl text-gray-600 mt-2 text-center">{{ date('F j, Y', $page->date) }} by {{ $page->author }}</h2>
