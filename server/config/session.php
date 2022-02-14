db75794e02af5d5d2190235b10a84bcc06d2e7af
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-02-13 20:56:16 -0600

    Expire session on browser close

--- server/config/session.php
+++ server/config/session.php
@@ -33,7 +33,7 @@ return [
 
     'lifetime' => env('SESSION_LIFETIME', 120),
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,
 
     /*
     |--------------------------------------------------------------------------
