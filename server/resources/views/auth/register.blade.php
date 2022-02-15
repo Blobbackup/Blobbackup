8b68a4a47d78a3f3b3f444df2fe65a896e096c18
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-02-14 18:37:11 -0600

    Make font a little darker

--- server/resources/views/auth/register.blade.php
+++ server/resources/views/auth/register.blade.php
@@ -20,16 +20,16 @@
                 @csrf
                 <input type="email" name="email" id="email" placeholder="Email" class="w-full border border-gray-400 rounded-full px-4 py-1" value="{{ old('email') }}" required />
                 <input type="password" name="password" id="password" placeholder="Password" class="w-full border border-gray-400 rounded-full px-4 py-1 mt-4" required />
-                <div class="text-center mt-4 text-xs text-gray-400">
+                <div class="text-center mt-4 text-xs text-gray-500">
                     All your backups will be encrypted using this password. Pick a good one and do not forget it. There is no way to 
                     recover this password if you forget it.
                 </div>
                 <input type="password" name="password_confirmation" id="passwordconfirmation" placeholder="Confirm Password" class="w-full border border-gray-400 rounded-full px-4 py-1 mt-4" required />
                 <button type="submit" class="bg-gray-200 rounded-full w-full py-2 font-bold mt-4">Start Trial</button>
                 <div class="text-center mt-4 text-xs">
-                    <div class="text-gray-400">By proceeding, you agree to the Blobbackup</div> 
-                    <a href="https://blobbackup.com/terms" class="text-blue-400">Terms of Service</a> and
-                    <a href="https://blobbackup.com/privacy" class="text-blue-400">Privacy Policy</a>.
+                    <div class="text-gray-500">By proceeding, you agree to the Blobbackup</div> 
+                    <a href="https://blobbackup.com/terms" class="text-blue-500">Terms of Service</a> and
+                    <a href="https://blobbackup.com/privacy" class="text-blue-500">Privacy Policy</a>.
                 </div>
             </form>
         </div>
