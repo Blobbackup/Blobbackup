bc43ad8705369f934bb2249532d9c806c3af3ef9
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-02-22 18:02:18 -0600

    Log display

--- client/src/blobbackup/backupstarteddialog.py
+++ client/src/blobbackup/backupstarteddialog.py
@@ -4,6 +4,7 @@ from PyQt6.QtGui import QIcon
 from blobbackup.ui.backupstarteddialog import Ui_BackupStartedDialog
 
 from blobbackup.util import LOGO_PATH, CHECK_PATH, get_pixmap
+from blobbackup.logger import get_logger
 
 
 class BackupStartedDialog(QDialog, Ui_BackupStartedDialog):
@@ -12,9 +13,12 @@ class BackupStartedDialog(QDialog, Ui_BackupStartedDialog):
         Ui_BackupStartedDialog.__init__(self)
         self.setupUi(self)
 
+        self.logger = get_logger()
+
         self.setWindowIcon(QIcon(LOGO_PATH))
 
         self.logo_label.setPixmap(get_pixmap(LOGO_PATH, 20, 20))
         self.check_label.setPixmap(get_pixmap(CHECK_PATH, 20, 20))
 
         self.close_button.pressed.connect(self.accept)
+        self.logger.info("Backup started dialog displayed")
