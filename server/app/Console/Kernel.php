86e81a4e2664c680b94fe85845609a103b58966a
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-02-27 06:38:41 -0600

    Change paddle modifier update to daily

--- server/app/Console/Kernel.php
+++ server/app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
                     $subscription->newModifier($amount)->create();
                 }
             }
-        })->hourly();
+        })->daily();
         $schedule->call(function () {
             foreach (Computer::all() as $computer) {
                 $last_backed_up = $computer->last_backed_up_at;
