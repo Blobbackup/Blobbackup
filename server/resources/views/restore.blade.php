6713ba58711d3214b08ba9a110d76299c11cbb49
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-02-28 18:14:12 -0600

    1.0.7

--- server/resources/views/restore.blade.php
+++ server/resources/views/restore.blade.php
@@ -14,9 +14,9 @@
         <h2 class="font-bold">Option 2: Restore to a Computer Where You Don’t Have Blobbackup Installed</h2>
         <div class="mt-4">
             Step 1: Download Blobbackup:
-            <a href="{{ asset('bin/blobbackup-darwin-amd-1.0.6.dmg') }}" class="text-blue-600 underline">Intel Mac</a> |
-            <a href="{{ asset('bin/blobbackup-darwin-arm-1.0.6.dmg') }}" class="text-blue-600 underline">M1 Mac</a> |
-            <a href="{{ asset('bin/blobbackup-win-1.0.6.exe') }}" class="text-blue-600 underline">Windows</a>.
+            <a href="{{ asset('bin/blobbackup-darwin-amd-1.0.7.dmg') }}" class="text-blue-600 underline">Intel Mac</a> |
+            <a href="{{ asset('bin/blobbackup-darwin-arm-1.0.7.dmg') }}" class="text-blue-600 underline">M1 Mac</a> |
+            <a href="{{ asset('bin/blobbackup-win-1.0.7.exe') }}" class="text-blue-600 underline">Windows</a>.
         </div>
         <div class="mt-4">
             Step 2: Install Blobbackup on your computer. Step by step instructions:
