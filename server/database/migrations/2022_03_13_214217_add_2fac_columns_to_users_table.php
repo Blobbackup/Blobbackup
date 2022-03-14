41e9ae2e90a1e4341b9ae793df616706589fac26
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-03-13 19:43:50 -0500

    Add 2fac and backup codes columns

--- server/database/migrations/2022_03_13_214217_add_2fac_columns_to_users_table.php
+++ server/database/migrations/2022_03_13_214217_add_2fac_columns_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddPhoneAndTotpSecretToUsersTable extends Migration
+class Add2FacColumnsToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,6 +14,8 @@ class AddPhoneAndTotpSecretToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->boolean('twofac')->default(false);
+            $table->string('backup_code')->nullable();
             $table->string('phone')->nullable();
             $table->string('totp_secret')->nullable();
         });
@@ -27,6 +29,8 @@ class AddPhoneAndTotpSecretToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('twofac');
+            $table->dropColumn('backup_code');
             $table->dropColumn('phone');
             $table->dropColumn('totp_secret');
         });
