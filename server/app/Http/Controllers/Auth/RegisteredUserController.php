43e45a647cfad23e51b949ec2bfdf4afd0fcd42a
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-03-15 12:16:27 -0500

    Remove unnecessary import

--- server/app/Http/Controllers/Auth/RegisteredUserController.php
+++ server/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Util\Util;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
