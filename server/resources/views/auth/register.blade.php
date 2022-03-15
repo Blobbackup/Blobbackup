dfaef5e75409109590543c677c6f680554bf1db3
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-03-14 22:27:29 -0500

    Update copy

--- server/resources/views/auth/register.blade.php
+++ server/resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 @section('title')
     @if(isset($leader))
-        Join Group
+        Start Backing Up
     @else
         Start Trial
     @endif
@@ -14,13 +14,13 @@
             </a>
             <h2 class="text-gray-600 mt-2">
                 @if(isset($leader))
-                    You're joining a group owned by '{{ $leader->email }}'.
+                    Group owner '{{ $leader->email }}' invited you to their group.
                 @else
                     Free 30 day trial. No card required.
                 @endif
             </h2>
             @if(isset($leader))
-                <div class="bg-gray-100 border-gray-700 text-gray-700 text-xs border-1 text-center my-2 py-2">
+                <div class="bg-blue-100 border-blue-500 text-blue-600 text-sm border-1 text-center mt-2 mb-4 py-2 rounded">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 inline-block mr-1 -mt-1" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
                     </svg>
@@ -46,7 +46,13 @@
                 @if(isset($leader))
                     <input type="hidden" name="leader_id" value="{{ $leader->id }}"/>
                 @endif
-                <button @if(auth()->check()) disabled @endif type="submit" class="bg-gray-200 rounded-full w-full py-2 font-bold mt-4">Start Trial</button>
+                <button @if(auth()->check()) disabled @endif type="submit" class="bg-gray-200 rounded-full w-full py-2 font-bold mt-4">
+                    @if(isset($leader))
+                        Start Backing Up
+                    @else
+                        Start Trial
+                    @endif
+                </button>
                 <div class="text-center mt-4 text-xs">
                     <div class="text-gray-500">By proceeding, you agree to the Blobbackup</div> 
                     <a href="https://blobbackup.com/terms" class="text-blue-500">Terms of Service</a> and
