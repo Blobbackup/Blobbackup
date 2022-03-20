c2b8ec744abe5a237b0848672165e689ca7a9d8f
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-03-20 09:54:12 -0500

    Enable groups by default for new users

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -23,7 +23,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'email',
         'password',
         'leader_id',
-        'status'
+        'status',
+        'groups'
     ];
 
     /**
