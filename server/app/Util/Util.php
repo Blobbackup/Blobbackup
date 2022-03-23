1e5b82e3feda2001ad7918f9f24e1f5d07966fba
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-03-22 23:18:24 -0500

    Update version on server

--- server/app/Util/Util.php
+++ server/app/Util/Util.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Mail;
 class Util
 {
     public static $perComputerPrice = 9;
-    public static $clientVersion = "1.1.0";
+    public static $clientVersion = "1.1.2";
 
     public static function formatBytes($size, $precision = 2)
     {
