ccdf8411b4b5b5c3a6e6f2c9b1d97a26ef9ea978
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-03-25 16:51:36 -0500

    Give paddle time to propagate on update

--- server/resources/views/payment.blade.php
+++ server/resources/views/payment.blade.php
@@ -33,7 +33,7 @@
                     @endif
                 </div>
                 <div class="text-gray-600 mt-2">
-                    <x-paddle-button :url="$payLink" data-theme="none" data-success="{{ route('payment') }}">
+                    <x-paddle-button :url="$payLink" data-theme="none" data-success="{{ route('payment') }}?checkout=updated">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block mr-1" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
                             <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
