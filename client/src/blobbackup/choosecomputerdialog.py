5108f2537117f6c43a6509e4444be7a6228995a9
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-03-26 19:18:14 -0500

    Don't include current computer in choose computer dialog

--- client/src/blobbackup/choosecomputerdialog.py
+++ client/src/blobbackup/choosecomputerdialog.py
@@ -5,6 +5,7 @@ from blobbackup.ui.choosecomputerdialog import Ui_ChooseComputerDialog
 from blobbackup.util import LOGO_PATH
 from blobbackup.api import get_computers
 from blobbackup.logger import get_logger
+from blobbackup.config import config
 
 
 class ChooseComputerDialog(QDialog, Ui_ChooseComputerDialog):
@@ -21,7 +22,10 @@ class ChooseComputerDialog(QDialog, Ui_ChooseComputerDialog):
         self.setWindowIcon(QIcon(LOGO_PATH))
 
         for computer in get_computers(email, password):
-            self.computers_combo_box.addItem(computer["name"], userData=computer["id"])
+            if str(computer["id"]) != config["meta"]["computer_id"]:
+                self.computers_combo_box.addItem(
+                    computer["name"], userData=computer["id"]
+                )
 
         self.continue_button.pressed.connect(self.accept)
         self.logger.info("Choose computer dialog displayed.")
