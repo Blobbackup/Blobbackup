7f8c9108586093fa9b9a2b7fb57e774b5a1a3111
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-04-06 09:14:46 -0500

    Bump version number

--- server/app/Util/Util.php
+++ server/app/Util/Util.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Mail;
 class Util
 {
     public static $perComputerPrice = 9;
-    public static $clientVersion = "1.1.6";
+    public static $clientVersion = "1.1.7";
 
     public static function formatBytes($size, $precision = 2)
     {
