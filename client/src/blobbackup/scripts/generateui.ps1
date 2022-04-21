d81ccca4ae9f0c14942c1c2be6639b2f8b9747fe
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-04-21 16:12:43 -0500

    Add missing ui file generation

--- client/src/blobbackup/scripts/generateui.ps1
+++ client/src/blobbackup/scripts/generateui.ps1
@@ -4,5 +4,6 @@ pyuic6 src/blobbackup/ui/backupstarteddialog.ui -o src/blobbackup/ui/backupstart
 pyuic6 src/blobbackup/ui/settingsdialog.ui -o src/blobbackup/ui/settingsdialog.py
 pyuic6 src/blobbackup/ui/mainwindow.ui -o src/blobbackup/ui/mainwindow.py
 pyuic6 src/blobbackup/ui/restoredialog.ui -o src/blobbackup/ui/restoredialog.py
+pyuic6 src/blobbackup/ui/choosecomputerdialog.ui -o src/blobbackup/ui/choosecomputerdialog.py
 pyuic6 src/blobbackup/ui/requestfulldiskdialog.ui -o src/blobbackup/ui/requestfulldiskdialog.py
 pyuic6 src/blobbackup/ui/loadingdialog.ui -o src/blobbackup/ui/loadingdialog.py
