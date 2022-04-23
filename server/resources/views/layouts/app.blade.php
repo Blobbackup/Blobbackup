80b5bd6b332f60fa33f64694c30999f80d862a36
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-04-23 06:45:02 -0500

    Use already loaded tailwindcss

--- server/resources/views/layouts/app.blade.php
+++ server/resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta name="robots" content="noindex,nofollow"/>
         <title>@yield('title') - Blobbackup</title>
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link rel="shortcut icon" type="image/jpg" href="{{ asset('img/logo.png') }}"/>
         <script src="{{ asset('js/jquery.min.js') }}"></script>
         <script src="{{ asset('js/sjcl.min.js') }}"></script>
