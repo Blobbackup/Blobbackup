150eb8be668fc5dc70a80c64cb5f9d5dc85762e0
Bimba Shrestha <bimbaemail@gmail.com>
2022-04-26 15:45:10 -0500

    Move repo handling code to changepasswordthread

--- client/src/blobbackup/changepasswordthread.py
+++ client/src/blobbackup/changepasswordthread.py
@@ -1,11 +1,21 @@
+import os
+import json
+import subprocess
+import tempfile
+
 from PyQt6.QtCore import QThread, pyqtSignal
 
-from blobbackup.loginthread import (
-    add_new_password_to_repo,
-    unlock_repo,
-    remove_all_but_new_password_from_repo,
-)
 from blobbackup.api import changepassword, get_computers
+from blobbackup.util import (
+    CREATE_NO_WINDOW,
+    is_windows,
+    is_mac,
+    get_restic_add_password_command,
+    get_restic_list_passwords_command,
+    get_restic_delete_password_command,
+    get_restic_unlock_command,
+    get_restic_env,
+)
 
 
 class ChangePasswordThread(QThread):
@@ -34,3 +44,64 @@ class ChangePasswordThread(QThread):
             add_new_password_to_repo(computer, self.password, self.new_password)
             remove_all_but_new_password_from_repo(computer, self.new_password)
         self.finished.emit(True)
+
+
+def add_new_password_to_repo(computer, old_password, password):
+    with tempfile.TemporaryDirectory() as root:
+        password_file = os.path.join(root, "password.txt")
+        with open(password_file, "w", encoding="utf-8") as f:
+            f.write(password)
+        if is_windows():
+            subprocess.run(
+                get_restic_add_password_command(password_file),
+                env=get_restic_env(computer, old_password),
+                creationflags=CREATE_NO_WINDOW,
+            )
+        elif is_mac():
+            subprocess.run(
+                get_restic_add_password_command(password_file),
+                env=get_restic_env(computer, old_password),
+            )
+
+
+def unlock_repo(computer, password):
+    if is_windows():
+        subprocess.run(
+            get_restic_unlock_command(),
+            env=get_restic_env(computer, password),
+            creationflags=CREATE_NO_WINDOW,
+        )
+    elif is_mac():
+        subprocess.run(
+            get_restic_unlock_command(),
+            env=get_restic_env(computer, password),
+        )
+
+
+def remove_all_but_new_password_from_repo(computer, password):
+    if is_windows():
+        ret = subprocess.run(
+            get_restic_list_passwords_command(),
+            env=get_restic_env(computer, password),
+            stdout=subprocess.PIPE,
+            creationflags=CREATE_NO_WINDOW,
+        ).stdout
+    elif is_mac():
+        ret = subprocess.run(
+            get_restic_list_passwords_command(),
+            env=get_restic_env(computer, password),
+            stdout=subprocess.PIPE,
+        ).stdout
+    keys = json.loads(ret)
+    for key in sorted(keys, key=lambda k: k["created"])[:-1]:
+        if is_windows():
+            subprocess.run(
+                get_restic_delete_password_command(key["id"]),
+                env=get_restic_env(computer, password),
+                creationflags=CREATE_NO_WINDOW,
+            )
+        elif is_mac():
+            subprocess.run(
+                get_restic_delete_password_command(key["id"]),
+                env=get_restic_env(computer, password),
+            )
--- client/src/blobbackup/loginthread.py
+++ client/src/blobbackup/loginthread.py
@@ -1,25 +1,9 @@
-import os
-import json
-import subprocess
-import tempfile
-
 from PyQt6.QtCore import QThread, pyqtSignal
 
-from blobbackup.api import login, get_computer
+from blobbackup.api import login
 from blobbackup.config import config, save_config
 from blobbackup.logger import get_logger
-from blobbackup.util import (
-    CREATE_NO_WINDOW,
-    is_windows,
-    is_mac,
-    get_password_from_keyring,
-    get_restic_add_password_command,
-    get_restic_list_passwords_command,
-    get_restic_delete_password_command,
-    get_restic_unlock_command,
-    save_password_in_keyring,
-    get_restic_env,
-)
+from blobbackup.util import save_password_in_keyring
 
 
 class LoginThread(QThread):
@@ -54,64 +38,3 @@ def update_email_and_password(email, password):
     config["meta"]["email"] = email
     save_config()
     save_password_in_keyring(password)
-
-
-def add_new_password_to_repo(computer, old_password, password):
-    with tempfile.TemporaryDirectory() as root:
-        password_file = os.path.join(root, "password.txt")
-        with open(password_file, "w", encoding="utf-8") as f:
-            f.write(password)
-        if is_windows():
-            subprocess.run(
-                get_restic_add_password_command(password_file),
-                env=get_restic_env(computer, old_password),
-                creationflags=CREATE_NO_WINDOW,
-            )
-        elif is_mac():
-            subprocess.run(
-                get_restic_add_password_command(password_file),
-                env=get_restic_env(computer, old_password),
-            )
-
-
-def unlock_repo(computer, password):
-    if is_windows():
-        subprocess.run(
-            get_restic_unlock_command(),
-            env=get_restic_env(computer, password),
-            creationflags=CREATE_NO_WINDOW,
-        )
-    elif is_mac():
-        subprocess.run(
-            get_restic_unlock_command(),
-            env=get_restic_env(computer, password),
-        )
-
-
-def remove_all_but_new_password_from_repo(computer, password):
-    if is_windows():
-        ret = subprocess.run(
-            get_restic_list_passwords_command(),
-            env=get_restic_env(computer, password),
-            stdout=subprocess.PIPE,
-            creationflags=CREATE_NO_WINDOW,
-        ).stdout
-    elif is_mac():
-        ret = subprocess.run(
-            get_restic_list_passwords_command(),
-            env=get_restic_env(computer, password),
-            stdout=subprocess.PIPE,
-        ).stdout
-    keys = json.loads(ret)
-    for key in sorted(keys, key=lambda k: k["created"])[:-1]:
-        if is_windows():
-            subprocess.run(
-                get_restic_delete_password_command(key["id"]),
-                env=get_restic_env(computer, password),
-                creationflags=CREATE_NO_WINDOW,
-            )
-        elif is_mac():
-            subprocess.run(
-                get_restic_delete_password_command(key["id"]),
-                env=get_restic_env(computer, password),
-            )
