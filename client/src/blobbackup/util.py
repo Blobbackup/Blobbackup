c702801305530fc3bbb2eba38a013f84e5401af5
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-04-26 15:09:28 -0500

    Remove all locks

--- client/src/blobbackup/util.py
+++ client/src/blobbackup/util.py
@@ -168,7 +168,7 @@ def get_restic_backup_command(max_upload_kibs, backup_connected_file_systems):
 
 
 def get_restic_unlock_command():
-    return [RESTIC_PATH, "unlock"]
+    return [RESTIC_PATH, "unlock", "--remove-all"]
 
 
 def get_restic_list_passwords_command():
