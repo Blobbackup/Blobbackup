70980cc2d637d9d4e4f988ed11073cdc0ef3e793
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-08-05 16:11:10 -0400

    Replace cd - with cd ../

--- client/src/blobbackup/scripts/buildrestic.ps1
+++ client/src/blobbackup/scripts/buildrestic.ps1
@@ -2,6 +2,6 @@ rm -r -fo restic
 git clone -b v0.12.1 https://github.com/restic/restic restic
 cd restic
 go run build.go
-cd -
+cd ../
 cp restic/restic src/blobbackup/bin/blobbackup.exe
 rm -r -fo restic
