2df5832db1c8824cbd219f009a6eaec6215f3160
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-08-05 16:12:36 -0400

    Copy restic.exe instead of restic for windows

--- client/src/blobbackup/scripts/buildrestic.ps1
+++ client/src/blobbackup/scripts/buildrestic.ps1
@@ -3,5 +3,5 @@ git clone -b v0.12.1 https://github.com/restic/restic restic
 cd restic
 go run build.go
 cd ../
-cp restic/restic src/blobbackup/bin/blobbackup.exe
+cp restic/restic.exe src/blobbackup/bin/blobbackup.exe
 rm -r -fo restic
