b7a12f21fdf286e97cb36764dae41e2f23292aa2
Bimba Shrestha <bshrestha.msae@gmail.com>
2023-11-04 11:56:04 -0400

    Increase price to keep up with storage costs

--- server/app/Util/Util.php
+++ server/app/Util/Util.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Mail;
 
 class Util
 {
-    public static $perComputerPrice = 9;
+    public static $perComputerPrice = 12;
     public static $clientVersion = "1.2.5";
     public static $trialLengthDays = 15;
     public static $deletedComputerRetentionDays = 15;
